bf4bb4153b4e7641c69fdade4f69e5574e941dff
Thalisson Estrela <thalissondev@gmail.com>
2016-10-17 14:58:03 -0200

    Init project

---
+++ app/src/main/java/testing/example/com/Calculator.java
@@ -0,0 +1,23 @@
+package testing.example.com;
+
+/**
+ * Created by Team Fretebras Android on 17/10/16.
+ */
+
+public class Calculator {
+    public double sum(double a, double b) {
+        return 0;
+    }
+
+    public double subtract(double a, double b) {
+        return 0;
+    }
+
+    public double divide(double a, double b) {
+        return 0;
+    }
+
+    public double multiply(double a, double b) {
+        return 0;
+    }
+}
---
+++ app/src/test/java/testing/example/com/CalculatorTest.java
@@ -0,0 +1,39 @@
+package testing.example.com;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Team Fretebras Android on 17/10/16.
+ */
+public class CalculatorTest {
+    private Calculator calculator;
+
+    @Before
+    public void setUp() throws Exception {
+        calculator = new Calculator();
+    }
+
+    @Test
+    public void sum() throws Exception {
+        assertEquals(6d, calculator.sum(1d, 5d), 0);
+    }
+
+    @Test
+    public void subtract() throws Exception {
+        assertEquals(1d, calculator.subtract(5d, 4d), 0);
+    }
+
+    @Test
+    public void divide() throws Exception {
+        assertEquals(4d, calculator.divide(20d, 5d), 0);
+    }
+
+    @Test
+    public void multiply() throws Exception {
+        assertEquals(10d, calculator.multiply(2d, 5d), 0);
+    }
+
+}
